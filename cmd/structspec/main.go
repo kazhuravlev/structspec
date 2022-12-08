6a9adfc6d3aa06cfc57d6de2332a524724350671
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2022-12-08 11:23:10 +0100

    cli wrapper

---
+++ cmd/structspec/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"github.com/kazhuravlev/structspec/internal/impl"
+	"github.com/urfave/cli/v3"
+	"os"
+)
+
+const (
+	argSrc            = "src"
+	argIncludeStructs = "structs"
+	argIgnoreStructs  = "ignore"
+	argTag            = "tag"
+	argOutFilename    = "out-file"
+	argOutPackage     = "out-pkg"
+)
+
+var (
+	version = "__from_source__"
+)
+
+func main() {
+	a := cli.NewApp()
+	// TODO: get from CI
+	a.Version = version
+	a.Name = "structspec"
+	a.Usage = "Generate structs specification. github.com/kazhuravlev/structspec"
+	a.Commands = []*cli.Command{
+		{
+			Name:   "gen",
+			Usage:  "Generate and write result",
+			Action: cmdGenerate,
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:     argSrc,
+					Value:    "",
+					Usage:    "Source directory",
+					Required: true,
+				},
+				&cli.StringSliceFlag{
+					Name:     argIncludeStructs,
+					Value:    nil,
+					Usage:    "Which structs should be included. Default: all founded",
+					Required: false,
+				},
+				&cli.StringSliceFlag{
+					Name:     argIgnoreStructs,
+					Value:    nil,
+					Usage:    "Which structs should be ignored. Default: no one",
+					Required: false,
+				},
+				&cli.StringSliceFlag{
+					Name:     argTag,
+					Value:    nil,
+					Usage:    "Which tags should be used for generation. Default: all founded",
+					Required: true,
+				},
+				&cli.StringFlag{
+					Name:     argOutFilename,
+					Value:    "spec_generated.go",
+					Usage:    "Output filename",
+					Required: true,
+				},
+				&cli.StringFlag{
+					Name:     argOutPackage,
+					Value:    "",
+					Usage:    "Output package name",
+					Required: true,
+				},
+			},
+		},
+	}
+
+	if err := a.Run(os.Args); err != nil {
+		panic("cannot run command: " + err.Error())
+	}
+}
+
+func cmdGenerate(c *cli.Context) error {
+	sourceDirectory := c.String(argSrc)
+	outFilename := c.String(argOutFilename)
+	outPackage := c.String(argOutPackage)
+	includeStructs := c.StringSlice(argIncludeStructs)
+	ignoreStructs := c.StringSlice(argIgnoreStructs)
+	tags := c.StringSlice(argTag)
+
+	err := impl.Generate(impl.NewOptions(
+		impl.WithSource(sourceDirectory),
+		impl.WithOutFilename(outFilename),
+		impl.WithOutPackage(outPackage),
+		impl.WithIncludeStructs(includeStructs),
+		impl.WithIgnoreStructs(ignoreStructs),
+		impl.WithTags(tags),
+	))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
